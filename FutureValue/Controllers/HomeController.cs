bc4307d8bf77582294ce611e7222c42d82a64450
Jonathon Nagassar <jonnagassar@live.com>
2020-12-15 15:53:34 -0500

    v1.0

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FutureValue.Models
+{
+    public class FutureValueModel
+    {
+        [Required(ErrorMessage ="Please enter a monthly investment.")]
+        [Range(1,500, ErrorMessage =
+            "Monthly investment amount must be between 1 and 500.")]
+        public decimal? MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Please enter a yearly interest rate.")]
+        [Range(0.1, 10.0, ErrorMessage =
+            "Yearly interest rate must be between 0.1 and 10.0")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number of years.")]
+        [Range(1, 50, ErrorMessage =
+            "Number of years must be between 1 and 50.")]
+        public int? Years { get; set; }
+        public decimal? CalculateFutureValue()
+        {
+            int? months = Years * 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal? futureValue = 0;
+            for (int i = 0; i< months ; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+
+            }
+            return futureValue;
+        }
+
+    }
+}
